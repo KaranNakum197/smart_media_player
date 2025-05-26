5b559061f44db775a2cb4f82ba7a8a9df67eec50
KaranNakum197 <karannakum456@gmail.com>
2025-05-26 23:54:40 +0530

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -20,7 +20,7 @@ def collect_images(gesture_name):
         ret, frame = cap.read()
         if not ret:
             break
-
+# main frame of focus
         frame = cv2.flip(frame, 1)
         cv2.rectangle(frame, (50, 50), (250, 250), (0, 255, 0), 2)
         cv2.putText(frame, f"{gesture_name}: {count}/{num_samples}", (50, 40),
